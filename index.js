4d60d41deace682020585fb6235a7eedd9fa51e1
Emad Albalawi <91331130+iemad00@users.noreply.github.com>
2024-06-05 18:57:23 +0300

    Configuring the environments

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ app.use(express.json());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 const port = 3000;
+
+
+// Setting up the correct environment
+if(!process.env.NODE_ENV)
+    process.env.NODE_ENV = 'development'
+require('dotenv').config({ path: `./environments/.env.${process.env.NODE_ENV}` })
+
 
 // ======== Routes ==========
 const moviesRouter = require('./routes/movies');
@@ -17,5 +23,6 @@ app.use('/movies', moviesRouter);
 
 
 app.listen(port, () => {
+    console.log(process.env.MONGO_CONNECTION_STRING);
     console.log(`The Server is running on port: ${port}`);
 })
