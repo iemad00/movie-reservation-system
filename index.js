8e9929f45e2cb54397aee00e7e62d64e232f1394
Emad Albalawi <91331130+iemad00@users.noreply.github.com>
2024-06-05 18:47:00 +0300

    Movies Route

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express")
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(express.json());
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello")
-})
+// ======== Routes ==========
+const moviesRouter = require('./routes/movies');
+app.use('/movies', moviesRouter);
+
+
 
 app.listen(port, () => {
     console.log(`The Server is running on port: ${port}`);
