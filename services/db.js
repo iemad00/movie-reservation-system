9a4ee7ee5b1855df2dea9ddb8d743c055230727d
Emad Albalawi <91331130+iemad00@users.noreply.github.com>
2024-06-07 05:29:27 +0300

    remove deprecated option

--- services/db.js
+++ services/db.js
@@ -3,13 +3,11 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
-        useNewUrlParser: true
-    });
+    await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB: ", error);
-    process.exit(1); // Exit process with failure
+    process.exit(1); 
   }
 };
 
